7f539613e35d4830690d830a63a7a23ad9604519
Lucy Mitchell <lucymhodges@gmail.com>
2018-04-26 16:13:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string) return  
+  if (string.toUpperCase() === string) return `YES INDEED!` 
 }
